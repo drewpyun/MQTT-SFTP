49b3f9e253273ab610cb7db03be9e4a466fe9636
drewpyun <48503175+drewpyun@users.noreply.github.com>
2023-10-22 21:01:59 -0400

    Update SFTP-PKI-TEST.py

--- test/SFTP-PKI-TEST.py
+++ test/SFTP-PKI-TEST.py
@@ -19,14 +19,22 @@ def save_config(config):
 config = load_config()
 
 # Function to get configuration from the user
-def get_config(prompt, key):
+def get_config(prompt, key, default_value=None):
     while True:
         print(f"What is the {prompt}?")
         for index, value in enumerate(config[key]):
             print(f"{index + 1}) {value}")
         print(f"{len(config[key]) + 1}) Add new {prompt}")
+        if default_value:
+            print(f"(Press Enter for default path: {default_value})")
+        
+        choice_str = input("Enter your choice: ")
+        
+        if default_value and choice_str == '':
+            return default_value
+
         try:
-            choice = int(input("Enter your choice: "))
+            choice = int(choice_str)
             if choice == len(config[key]) + 1:
                 new_value = input(f"Enter new {prompt}: ")
                 config[key].append(new_value)
@@ -41,6 +49,6 @@ def get_config(prompt, key):
 ip = get_config('IP of the SFTP server', 'ip')
 username = get_config('username of the SFTP server', 'username')
 password = get_config('password of the SFTP server', 'password')
-private_key_path = get_config('path of the private key', 'private_key_path')
+private_key_path = get_config('path of the private key', 'private_key_path', default_value='~/.ssh/')
 
 print(f"Selected configurations: IP: {ip}, Username: {username}, Password: {password}, Private Key Path: {private_key_path}")
