d9e59d6b0e6d1712719b57c4413403f4086dbb92
drewpyun <48503175+drewpyun@users.noreply.github.com>
2023-12-08 20:54:18 -0500

    Update broker.py

--- scripts/broker.py
+++ scripts/broker.py
@@ -1,6 +1,10 @@
 import paho.mqtt.client as mqtt
 import paramiko
 import json
+import logging
+
+# Logging Configuration
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # MQTT Broker Configuration
 broker_address = "localhost"
@@ -17,19 +21,21 @@ command_topic = "iot/sftp/command"
 response_topic = "iot/sftp/response"
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    logging.info(f"Connected with result code {rc}")
     client.subscribe(command_topic)
 
 def on_message(client, userdata, msg):
-    print(f"Received command: {msg.payload.decode()}")
+    logging.info(f"Received command: {msg.payload.decode()}")
     try:
         command = json.loads(msg.payload.decode())
+        logging.debug(f"Parsed command: {command}")
 
         # Initialize SFTP client
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(sftp_host, port=sftp_port, username=sftp_username, key_filename=sftp_private_key)
         sftp = ssh.open_sftp()
+        logging.debug("SFTP client initialized and connected")
 
         # Execute command
         if command['action'] == 'get':
@@ -45,9 +51,12 @@ def on_message(client, userdata, msg):
         ssh.close()
 
         client.publish(response_topic, response)
+        logging.info(f"Response published: {response}")
 
     except Exception as e:
-        client.publish(response_topic, f"Error: {str(e)}")
+        error_message = f"Error: {str(e)}"
+        client.publish(response_topic, error_message)
+        logging.error(error_message)
 
 client = mqtt.Client()
 client.on_connect = on_connect
