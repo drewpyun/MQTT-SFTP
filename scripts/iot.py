7518b9fb8864dfad9b06237e673629af074d7a6c
drewpyun <48503175+drewpyun@users.noreply.github.com>
2023-12-10 05:59:24 -0500

    Update iot.py

--- scripts/iot.py
+++ scripts/iot.py
@@ -2,61 +2,72 @@ import paho.mqtt.client as mqtt
 import json
 import os
 
-# Function to prompt user for broker address
-def get_broker_address():
-    return input("Enter the MQTT broker address: ")
+# Function to prompt the user for input and validate it
+def get_input(prompt, validation_func=None):
+    while True:
+        user_input = input(prompt)
+        if not validation_func or validation_func(user_input):
+            return user_input
+        print("Invalid input. Please try again.")
 
-# MQTT Broker Configuration
-broker_address = get_broker_address()
-port = 1883  # Default MQTT port
+# Function to validate IP addresses
+def valid_ip(address):
+    parts = address.split(".")
+    if len(parts) != 4:
+        return False
+    for item in parts:
+        if not item.isdigit() or not 0 <= int(item) <= 255:
+            return False
+    return True
+
+# Prompt user for the MQTT broker's IP address
+broker_address = get_input("Enter the MQTT broker's IP address: ", valid_ip)
+port = 1883  # Standard MQTT port
 
 # MQTT Topics
 command_topic = "iot/sftp/command"
 response_topic = "iot/sftp/response"
 
-# Called when the client receives CONNACK response from the server
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe(response_topic)  # Subscribe to the response topic
+    print("Connected with result code " + str(rc))
+    client.subscribe(response_topic)
 
-# Called when a message has been received on a topic that the client subscribes to
 def on_message(client, userdata, msg):
     print(f"Response: {msg.payload.decode()}")
 
-# Create MQTT client instance
 client = mqtt.Client()
-
-# Assign event callbacks
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Connect to MQTT broker
 client.connect(broker_address, port, 60)
 
-# Start the network loop in a separate thread
-client.loop_start()
+# Define local and remote paths for the file transfer
+remote_path = '/remote/path/to/file'
+local_path = '/local/path/to/store'
 
-# Local path where the file will be stored
-local_path = '/home/testlaptop-1/Documents/example.txt'
-
-# Ensure the directory exists
+# Ensure the local directory exists and is writable
 local_dir = os.path.dirname(local_path)
 if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
+    try:
+        os.makedirs(local_dir)
+    except PermissionError:
+        print(f"Permission denied: Unable to create directory {local_dir}")
+        exit(1)
+else:
+    if not os.access(local_dir, os.W_OK):
+        print(f"Permission denied: Unable to write to directory {local_dir}")
+        exit(1)
 
-# Command to be sent to the broker - in this case, to download a file
+# Send command to perform file transfer action
 command = {
-    'action': 'get',  # Action type ('get' for download, 'put' for upload)
-    'remote_path': '/remote/path/to/file',  # Path of the file on the SFTP server
-    'local_path': local_path  # Local path where the file will be saved
+    'action': 'get',  # 'get' for download, 'put' for upload
+    'remote_path': remote_path,
+    'local_path': local_path
 }
-
-# Publish the command to the MQTT broker
 client.publish(command_topic, json.dumps(command))
 
-# Wait for user input before exiting
-input("Press Enter to exit...\n")
+client.loop_start()
 
-# Stop the network loop and disconnect from the broker
+input("Press Enter to exit...\n")
 client.loop_stop()
 client.disconnect()
