5c77b8ee61da0d879679f6baade69f18ee4b2ace
drewpyun <drew.pyun@gmail.com>
2023-12-11 01:54:44 -0500

    Update broker.py

--- scripts/broker.py
+++ scripts/broker.py
@@ -9,10 +9,10 @@ broker_address = "localhost"
 port = 1883
 
 # SFTP Server Configuration
-sftp_host = '10.0.1.194'
+sftp_host = 'sftp_server_ip'  # Update with your SFTP server IP
 sftp_port = 22
-sftp_username = 'test'
-sftp_private_key = '/home/testlaptop/.ssh/id_rsa_mqtt'
+sftp_username = 'sftp_username'  # Update with your SFTP username
+sftp_private_key = '/path/to/private/key'  # Update with your private key path
 
 # MQTT Topics
 command_topic = "iot/sftp/command"
@@ -23,56 +23,37 @@ file_transfer_topic = "iot/sftp/file_transfer"
 passphrase = getpass.getpass("Enter the passphrase for the private key: ")
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
     client.subscribe(command_topic)
 
 def on_message(client, userdata, msg):
     print(f"Received command: {msg.payload.decode()}")
     try:
         command = json.loads(msg.payload.decode())
-
-        # Initialize SFTP client
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        private_key = paramiko.RSAKey(filename=sftp_private_key, password=passphrase)
-        ssh.connect(sftp_host, port=sftp_port, username=sftp_username, pkey=private_key)
-        sftp = ssh.open_sftp()
-
-        # Execute command
         if command['action'] == 'get':
-            local_path = command['local_path']
-            # Ensure the directory exists
-            local_dir = os.path.dirname(local_path)
-            if not os.path.exists(local_dir):
-                os.makedirs(local_dir)
+            print(f"Downloading file from {command['remote_path']} to {command['local_path']}")
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            private_key = paramiko.RSAKey(filename=sftp_private_key, password=passphrase)
+            ssh.connect(sftp_host, port=sftp_port, username=sftp_username, pkey=private_key)
+            sftp = ssh.open_sftp()
+            sftp.get(command['remote_path'], command['local_path'])
+            sftp.close()
+            ssh.close()
 
-            sftp.get(command['remote_path'], local_path)
-            print(f"File downloaded successfully to {local_path}.")
-
-            # Read and send the file content to the IoT device via MQTT
-            with open(local_path, 'rb') as file:
+            # Read and send file content
+            with open(command['local_path'], 'rb') as file:
                 file_content = file.read()
                 client.publish(file_transfer_topic, file_content)
-                print("File content sent to the IoT device.")
-
-        elif command['action'] == 'put':
-            # This part would handle file upload if needed
-            pass
-
+                print("File content sent to IoT device.")
         else:
-            response = "Invalid command."
-            client.publish(response_topic, response)
-
-        sftp.close()
-        ssh.close()
-
+            client.publish(response_topic, "Invalid command.")
     except Exception as e:
-        print(f"Error encountered: {str(e)}")
+        print(f"Error: {str(e)}")
         client.publish(response_topic, f"Error: {str(e)}")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.connect(broker_address, port, 60)
 client.loop_forever()
