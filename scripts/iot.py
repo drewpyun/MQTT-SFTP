d94e498706767bedb65608793b96f3acd3b6bdb8
drewpyun <drew.pyun@gmail.com>
2023-12-11 01:54:54 -0500

    Update iot.py

--- scripts/iot.py
+++ scripts/iot.py
@@ -2,52 +2,47 @@ import paho.mqtt.client as mqtt
 import json
 import os
 
-# Function to prompt the user for input and validate it
-def get_input(prompt, validation_func=None):
-    while True:
-        user_input = input(prompt)
-        if not validation_func or validation_func(user_input):
-            return user_input
-        print("Invalid input. Please try again.")
+# Function to prompt for broker's IP address
+def get_broker_address():
+    return input("Enter the MQTT broker's IP address: ")
 
-# Function to validate IP addresses
-def valid_ip(address):
-    parts = address.split(".")
-    if len(parts) != 4:
-        return False
-    for item in parts:
-        if not item.isdigit() or not 0 <= int(item) <= 255:
-            return False
-    return True
-
-# Prompt user for the MQTT broker's IP address
-broker_address = get_input("Enter the MQTT broker's IP address: ", valid_ip)
-port = 1883  # Standard MQTT port
+# MQTT Broker Configuration
+broker_address = get_broker_address()
+port = 1883
 
 # MQTT Topics
+command_topic = "iot/sftp/command"
 response_topic = "iot/sftp/response"
 file_transfer_topic = "iot/sftp/file_transfer"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe(response_topic)
-    client.subscribe(file_transfer_topic)
+    client.subscribe([response_topic, file_transfer_topic])
 
 def on_message(client, userdata, msg):
     if msg.topic == response_topic:
         print(f"Response: {msg.payload.decode()}")
     elif msg.topic == file_transfer_topic:
-        # File content received, write it to a local file
-        local_path = '/path/to/destination/file'  # Update this path as needed
-        with open(local_path, 'wb') as file:
-            file.write(msg.payload)
-            print(f"File received and saved to {local_path}")
+        print("Received file content.")
+        local_path = '/local/path/to/save/file'  # Update with your path
+        try:
+            with open(local_path, 'wb') as file:
+                file.write(msg.payload)
+                print(f"File successfully saved to {local_path}")
+        except Exception as e:
+            print(f"Error saving file: {e}")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(broker_address, port, 60)
+
+# Define and send the file transfer command
+remote_path = '/remote/path/to/file'  # Update with the file path on the SFTP server
+local_path = '/local/path/to/save/file'  # Update with the path where the file should be saved
+command = {'action': 'get', 'remote_path': remote_path, 'local_path': local_path}
+client.publish(command_topic, json.dumps(command))
 
 client.loop_start()
 input("Press Enter to exit...\n")
